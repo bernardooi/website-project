291e9c696f924638513da4a26bce40bd89c7da58
KenjiSakurai <kenji.sakurai@elev.ga.ntig.se>
2021-11-24 11:07:42 +0100

    1231312

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(window).on("load",function(){
     $(".loader-wrapper").fadeOut("slow");
 });
+
+
+
+var images = [
+    "Minecraft_cover.png",
+    "sekiro.png",
+    "batman.jpg"
+];
+
+
+function gamesiteload(i) {
+    document.getElementById("#pageimg").src = images[1];
+    document.getElementById("#pagetitle").style.visibility='visible';
+    document.getElementById("#pagescore").style.visibility='visible';
+    document.getElementById("#pagesynopsis").style.visibility='visible';
+
+}
