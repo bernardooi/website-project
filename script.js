c34da740d93a08ecca30bf86b4e32cdd4fc3d6f8
KenjiSakurai <kenji.sakurai@elev.ga.ntig.se>
2021-11-25 12:25:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ $("preimg").addEventListener("click", gamesiteload)
 
 
 function gamesiteload(i) {
-    document.getElementById("#pageimg").src = images[i];
-    document.getElementById("#pagetitle").style.visibility='visible';
-    document.getElementById("#pagescore").style.visibility='visible';
-    document.getElementById("#pagesynopsis").style.visibility='visible';
+    document.getElementById("pageimg").src = images[i];
+    document.getElementById("pagetitle").style.visibility='visible';
+    document.getElementById("pagescore").style.visibility='visible';
+    document.getElementById("pagesynopsis").style.visibility='visible';
 
 }
 
