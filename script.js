0fd13be216d478fae68a025250e8dcc67d561894
KenjiSakurai <kenji.sakurai@elev.ga.ntig.se>
2021-12-03 14:30:22 +0100

    bruhrehgrgbdvj h jf

--- script.js
+++ script.js
@@ -26,14 +26,48 @@ var names = [
     "Sekiro: Shadows Die Twice",
     "Smite",
     "Dark Souls Remastered",
-    "Star Wars Jedi Fallen Order",
+    "Star Wars: Jedi Fallen Order",
     "Batman: Arkham Knight",
     "League of Legends",
     "Age of Empires IV",
     "Europa Universalis IV"
 ];
 
-var score = []
+var score = [
+    "10",
+    "9",
+    "0",
+    "9",
+    "7",
+    "7",
+    "7",
+    "7",
+    "7"
+];
+
+var year = [
+    "2011",
+    "2019",
+    "2014",
+    "2018",
+    "2019",
+    "2015",
+    "2009",
+    "2021",
+    "2013"
+]
+
+var developer = [
+    "Mojang",
+    "From Software",
+    "Hi-Rez Studios",
+    "From Software",
+    "EA",
+    "Rocksteady Studios",
+    "Riot Games",
+    "Xbox Games Studios",
+    "Paradox Interactive"
+]
 
 //Slideshow
 
@@ -41,35 +75,15 @@ var i=0;
 
 console.log(i);
 
-function gamesiteload(i){
-document.getElementById("gpimg").src = images[i];
+function gamesiteload(i)
+{
+
+    document.getElementById("gpimg").src = images[i];
+
 }
 
 
 
-
-var BildGalleri = ["frame_0_delay-0.1s.gif", "frame_1_delay-0.1s.gif", "frame_2_delay-0.1s.gif", "frame_3_delay-0.1s.gif", "frame_4_delay-0.1s.gif", "frame_5_delay-0.1s.gif"];
-
-document.addEventListener("keydown", changePic)
-
-function changePic(){
-    if(event.key == "ArrowLeft"){
-        if(i<5){
-            i++;
-        }
-        else if(i==5){
-           i=0;
-        }
-    }
-
-    else if(event.key == "ArrowRight"){
-        if(i>0){
-            i--;
-        }
-        else if(i==0){
-            i=5;
-        }
-    }
 
 
     //document.getElementById("featuredimg")[0].src = BildGalleri[i];
